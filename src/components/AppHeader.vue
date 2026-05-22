45ddeef8f24bd5b19e05a06d4b69dd1c6cb063f3
arianetayna0-beep <arianetayna0@gmail.com>
2026-05-21 22:25:43 -0300

    página home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,14 @@
 <template>
     <header class="header-content">
-        <img src="../assets/logo.png" alt="Logo" class="logo">
-        <div class="header-links">
-            <router-link class="link" to="/">Início</router-link>
-            <router-link class="link" to="/sobre">Sobre</router-link>
-        </div>
-        <div class="buttons">
-            <button class="login-button" @click="router.push('/login')">Entrar</button>
-            <button class="signup-button" @click="router.push('/cadastro')">Cadastrar-se</button>
-            <i class="pi pi-align-justify" style="font-size: 2rem;"></i>
-        </div>
+            <img src="../assets/logo.png" alt="Logo" class="logo">
+            <div class="header-links">
+                <router-link class="link" to="/">Início</router-link>
+                <router-link class="link" to="/sobre">Sobre</router-link>
+            </div>
+            <div class="buttons">
+                <button class="login-button" @click="router.push('/login')">Entrar</button>
+                <i class="pi pi-align-justify" style="font-size: 2rem;"></i>
+            </div>
     </header>
 </template>
 
@@ -22,3 +21,57 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.header-content{
+    display: flex;
+    justify-content: space-around;
+    height: 6rem;
+    align-items: center;
+    background-color: #fff;
+    gap: 5rem;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.logo{
+    margin-top: 4rem;
+    width: 10rem;
+}
+
+.link{
+    text-decoration: none;
+    color: #000;
+    font-size: 1.2rem;
+}
+
+.header-links {
+    display: flex;
+    gap: 1rem;
+}
+
+.link:hover {
+    color: #88AB5D;
+}
+
+.buttons {
+    display: flex;
+    gap: 1rem;
+}
+
+.login-button{
+    width: 6rem;
+    height: 2rem;
+    border-radius: 1rem;
+    border: none;
+    background-color: #E5E5F0;
+    color: #88AB5D;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.login-button:hover {
+    background-color: #88AB5D;
+    color: #fff;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="home-content">
+  <div class="background">
     <AppHeader />
+    <div class="home-content">
+      <div class="conteudo">
+        <h1 class="titulo">segur<span>ON</span></h1>
+        <h2 class="subtitulo">Segurança que faz a diferença.</h2>
+      </div>
+    </div>
+    <div class="image-section">
+      <img src="../assets/Homesite2.jpg" alt="Imagem de Segurança" class="images-section">
+      <img src="../assets/Homesite1.jpg" alt="Imagem de Segurança" class="images-section">
+    </div>
   </div>
 </template>
 
 <script setup>
 import AppHeader from '../components/AppHeader.vue'
-import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router'
 </script>
+
 
 <style scoped>
 .home-content {
-  display: flex;
   width: 100%;
-  height: auto;
+  background-image: url(../assets/Homesite3.png);
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+}
+
+.images-section {
+  display: flex;
+  width: 30rem;
+  height: 20rem;
+  margin-top: 3rem;
+}
+
+.image-section{
+  display: flex;
+  justify-content: center;
+  gap: 5rem;
+  height: 30rem;
+  background-color: #3d8127;
+}
+
+
+.conteudo {
+  text-align: center;
+  margin-top: 8rem;
+  color: #ffffff;
+}
+
+.titulo {
+  font-size: 5rem;
+  font-weight: bold;
+  color: white;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.6);
+  
+}
+
+.titulo span {
+  color: #045203;
+}
+
+.botao{
+  width: 10rem;
+  height: 3rem;
+  border-radius: 1rem;
+  border: none;
+  color: #3d8127;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.botao:hover{
+  background-color: #3d8127;
+  color: white;
+  transition: 0.3s;
+}
+
+.subtitulo {
+  font-size: 40px;
+  font-weight: bold;
+  color: #000000;
+    /* Cria uma leve aura clara ao redor das letras para desgrudar do fundo escuro */
+  text-shadow: 0px 0px 40px rgba(255, 255, 255, 0.6); 
+}
+
+.botao {
+  margin-top: 20px;
+  font-size: 25px;
+}
+
+.sessao-verde {
+  background: #3d8127;
+  padding: 50px;
+}
+
+.imagem {
+  border-radius: 10px;
 }
 </style>
