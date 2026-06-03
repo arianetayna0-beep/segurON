ea932bab0397195ae5e3bdaa3301737c7d21b31e
fernandesgbr <fernandesgbr03@gmail.com>
2026-06-02 21:25:54 -0300

    sobre

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -3,50 +3,52 @@
     <AppHeader />
 
     <main class="sobre-container">
-    <div class="sobre-card">
-      <h1>Sobre o SegurON</h1>
+      <div class="sobre-card">
+        <h1>Sobre o SegurON</h1>
 
-      <p>
-        O <strong>SegurON</strong> é um sistema de controle de Equipamentos de
-        Proteção Individual (EPIs) desenvolvido pelos estudantes
-        <strong>Gabriela Fernandes</strong>,
-        <strong>Ariane Macedo</strong> e
-        <strong>Maicon Oliveira</strong>.
-      </p>
-
-      <p>
-        Este sistema foi criado para auxiliar empresas no gerenciamento e
-        controle de EPIs, oferecendo uma solução prática e eficiente para o
-        acompanhamento de estoque, entregas e conformidade com as normas de
-        segurança do trabalho.
-      </p>
-
-      <p>
-        Nosso objetivo é facilitar o controle de estoque, entrega e geração de
-        relatórios, contribuindo para uma gestão mais organizada e segura para
-        empresas e funcionários.
-      </p>
-
-      <h2>Principais Recursos</h2>
-
-      <ul>
-        <li>Gestão de funcionários e setores;</li>
-        <li>Controle rigoroso de estoque de EPIs;</li>
-        <li>Histórico de entregas e devoluções;</li>
-        <li>Relatórios rápidos para auditorias;</li>
-        <li>Rastreamento completo dos equipamentos entregues;</li>
-        <li>Maior controle e conformidade com normas de segurança.</li>
-      </ul>
-
-      <div class="missao">
-        <h2>Nossa Missão</h2>
         <p>
-          Promover uma gestão eficiente dos Equipamentos de Proteção Individual,
-          garantindo mais segurança, organização e praticidade no ambiente de
-          trabalho.
+          O <strong>SegurON</strong> é um sistema de controle de Equipamentos de
+          Proteção Individual (EPIs) desenvolvido pelos estudantes de 
+          <strong>Análise e Desenvolvimento de Sistemas</strong> 
+          pela Escola e Faculdade de Tecnologia Senai Roberto Mange:
+          <strong>Gabriela Fernandes</strong>,
+          <strong>Ariane Macedo</strong> e
+          <strong>Maicon Oliveira</strong>.
         </p>
+
+        <p>
+          Este sistema foi criado para auxiliar empresas no gerenciamento e
+          controle de EPIs, oferecendo uma solução prática e eficiente para o
+          acompanhamento de estoque, entregas e conformidade com as normas de
+          segurança do trabalho.
+        </p>
+
+        <p>
+          Nosso objetivo é facilitar o controle de estoque, entrega e geração de
+          relatórios, contribuindo para uma gestão mais organizada e segura para
+          empresas e funcionários.
+        </p>
+
+        <h2>Principais Recursos</h2>
+
+        <ul>
+          <li>Gestão de funcionários e setores;</li>
+          <li>Controle rigoroso de estoque de EPIs;</li>
+          <li>Histórico de entregas e devoluções;</li>
+          <li>Relatórios rápidos para auditorias;</li>
+          <li>Rastreamento completo dos equipamentos entregues;</li>
+          <li>Maior controle e conformidade com normas de segurança.</li>
+        </ul>
+
+        <div class="missao">
+          <h2>Nossa Missão</h2>
+          <p>
+            Promover uma gestão eficiente dos Equipamentos de Proteção Individual,
+            garantindo mais segurança, organização e praticidade no ambiente de
+            trabalho.
+          </p>
+        </div>
       </div>
-    </div>
     </main>
 
     <Footer />
